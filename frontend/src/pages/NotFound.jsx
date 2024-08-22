1a66a849a6385919f59e1e1c7bd3fe65554ffd58
Katerina Altshuler <katya.altshuler@f1cs.it>
2024-08-22 09:36:07 +0200

    first commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+
+function NotFound() {
+
+
+    return <div>
+        <h1>404 NOT FOUND</h1>
+
+    </div>
+
+}
+
+export default NotFound
