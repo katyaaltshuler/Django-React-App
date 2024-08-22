93a30acc8cb0ab68ea4d07f86c1c703c6ee78a3a
Katerina Altshuler <katya.altshuler@f1cs.it>
2024-08-22 11:05:38 +0200

    fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 import { ACCESS_TOKEN } from './constants'
 
-const apiUrl = "/choreo-apis/djangoreactapp/backend/v1"
+const apiUrl = "/choreo-apis/djangoreactapp/backend/v1";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL ?  import.meta.env.VITE_API_URL : apiUrl,
-})
+});
 
 api.interceptors.request.use(
     (config) => {
@@ -18,7 +18,7 @@ api.interceptors.request.use(
     (error) => {
         return Promise.reject(error)
     }
-)
+);
 
 
 export default api
